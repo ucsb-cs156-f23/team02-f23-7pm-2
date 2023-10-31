a69b2f719c48254612a37f958be228e1360a2fda
Andy Ouyang <andy136899@gmail.com>
2023-10-31 12:04:04 -0700

    fixed function name

--- src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
@@ -88,7 +88,7 @@ public class ArticlesController extends ApiController{
     @Operation(summary= "Delete an Article")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("")
-    public Object deleteUCSBDate(
+    public Object deleteArticles(
             @Parameter(name="id") @RequestParam Long id) {
         Articles article = articlesRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException(Articles.class, id));
@@ -96,4 +96,5 @@ public class ArticlesController extends ApiController{
         articlesRepository.delete(article);
         return genericMessage("Article with id %s deleted".formatted(id));
     }
+
 }
