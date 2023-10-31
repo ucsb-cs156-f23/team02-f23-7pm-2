dd0f0bde59fc8a1ea9d1ef76f59ac42c64e9eaaf
Andy Ouyang <andy136899@gmail.com>
2023-10-31 12:06:52 -0700

    changed function name

--- src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
@@ -88,7 +88,7 @@ public class ArticlesController extends ApiController{
     @Operation(summary= "Delete an Article")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("")
-    public Object deleteUCSBDate(
+    public Object deleteArticle(
             @Parameter(name="id") @RequestParam Long id) {
         Articles article = articlesRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException(Articles.class, id));
